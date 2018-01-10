73c1873cae2b2754ba7736ef38b65f5348c60b66
djdcann <djdcann@gmail.com>
2018-01-10 12:58:47 -0500

    add Stats

--- Stats.py
+++ Stats.py
@@ -1,14 +1,25 @@
 def fact(x):
-    if x == 1 or x == 0:
+    if x < 2:
         return 1
     else:
         return x*fact(x-1)
 
+def fact_break(x, y):
+    if y > abs(x):
+        raise Exception('incorrect arguments')
+    elif x == y:
+        return 1
+    else:
+        return x*fact_break(x-1, y)
+
 def nPr(n, r):
-    return fact(n) / fact(n-r)
+    return fact_break(n, n-r)
 
 def nCr(n, r):
+    if r > n-r:
+        r = n-r
     return nPr(n, r) / fact(r)
 
+
 if __name__ == '__main__':
-    print nCr(3, 2)
+    print nCr(3, 1)
