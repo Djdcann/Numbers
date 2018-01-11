7d6798ad375af4936f5a7cce500ed0616f4b4640
djdcann <djdcann@gmail.com>
2018-01-11 12:59:12 -0500

    update

--- Stats.py
+++ Stats.py
@@ -1,9 +1,11 @@
+#full factorial
 def fact(x):
     if x < 2:
         return 1
     else:
         return x*fact(x-1)
 
+#factorial that stops at a certain number
 def fact_break(x, y):
     if y > abs(x):
         raise Exception('incorrect arguments')
@@ -12,9 +14,11 @@ def fact_break(x, y):
     else:
         return x*fact_break(x-1, y)
 
+#permutations
 def nPr(n, r):
     return fact_break(n, n-r)
 
+#combinations
 def nCr(n, r):
     if r > n-r:
         r = n-r
