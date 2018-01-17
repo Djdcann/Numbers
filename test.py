e12092c1c64de5bb6ef8fdef26468b542684e662
djdcann <djdcann@gmail.com>
2018-01-17 11:23:54 -0500

    update

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import Stats as sts
+import primes as pr
 
 plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 plt.show()
 print 'lol'
 print sts.nCr(3, 2)
+print pr.primeSieve(100)
